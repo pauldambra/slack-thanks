7c0a0ad3e79f812ae1f81589ab8f5c3b59b1018c
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2018-10-26 16:12:55 +0100

    Add logging to thanks sending

--- src/send-thanks.js
+++ src/send-thanks.js
@@ -1,12 +1,16 @@
 const request = require('superagent')
 const logger = require('heroku-logger')
 
-module.exports = (responseUrl) => (command) => {
-  logger.info(`response url set to ${responseUrl}: processing ${command}`)
-  return request
-    .post(responseUrl)
-    .set('Content-type', 'application/json')
-    .send({
-      text: `someone wants to thank <${command.recipient}> for ${command.reason}`
-    })
+module.exports = (responseUrl) => {
+  logger.info(`setting up thanker with url: ${responseUrl}`)
+
+  return (command) => {
+    logger.info(`thanker processing ${command}`)
+    return request
+      .post(responseUrl)
+      .set('Content-type', 'application/json')
+      .send({
+        text: `someone wants to thank <${command.recipient}> for ${command.reason}`
+      })
+  }
 }
