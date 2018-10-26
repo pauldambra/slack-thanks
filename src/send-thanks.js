0f1f75e482edd2de0f6c7b0bf11b1c95e7f986de
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2018-10-26 14:59:01 +0100

    try to format the message text

--- src/send-thanks.js
+++ src/send-thanks.js
@@ -3,6 +3,8 @@ const request = require('superagent')
 module.exports = (responseUrl, command) => {
   return request
     .post(responseUrl)
-    .send('yo!') // sends a JSON post body
     .set('Content-type', 'application/json')
+    .send({
+      text: `someone wants to thank ${command.recipient} for ${command.reason}`
+    })
 }
